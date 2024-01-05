0836ebfd453d466871d0ae9488a91166132633df
Tun Khine <tunkhine126@gmail.com>
2024-01-04 19:40:31 -0500

    Initial commit

---
+++ config/tailwind.config.js
@@ -0,0 +1,39 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      textColor: {
+        primary: '#D4AF37',
+        secondary: '#595959',
+        qDgrey: '#262626',
+        qform: '#9C9CAB'
+      },
+      colors: {
+        appBG: '#619A46',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
